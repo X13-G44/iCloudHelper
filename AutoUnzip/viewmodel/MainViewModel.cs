4548d4de6e7694c59341adfa42ac0e8bb653614e
Chris <chris@x13-g44.com>
2025-10-06 17:14:32 +0200

    Added ViewModelBase for all ViewsModels. Add INotifyDataErrorInfo in ViewModelBase. Add Copyright header.

--- AutoUnzip/viewmodel/MainViewModel.cs
+++ AutoUnzip/viewmodel/MainViewModel.cs
@@ -1,4 +1,34 @@
-﻿using System;
+﻿/// ////////////////////////////////////////////////////////////////////////
+///
+/// Project:			iCloudHelper
+/// Project Source:	https://github.com/X13-G44/iCloudHelper
+///
+/// Author: 			Christian Harscher <info@x13-g44.com>
+/// Date:				06.10.2025
+///
+/// ////////////////////////////////////////////////////////////////////////
+/// 
+/// SPDX-License-Identifier: Apache-2.0
+/// Copyright (c) 2025 Christian Harscher (alias X13-G44)
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the Apache License as
+/// published by the Free Software Foundation, either version 2 of the
+/// License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// Apache License for more details.
+///
+/// You should have received a copy of the Apache License
+/// along with this program. If not, see <http://www.apache.org/licenses/LICENSE-2.0/>.
+///      
+/// ////////////////////////////////////////////////////////////////////////
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -23,7 +53,7 @@ using System.Windows.Threading;
 
 namespace AutoUnzip.viewmodel
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : ViewModelBase
     {
         const int MAX_FILES_TO_SHOW = 5;
 
@@ -83,13 +113,9 @@ namespace AutoUnzip.viewmodel
 
 
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-
-
-        private Dispatcher _Dispatcher;
-        private Window _View;
-        private List<String> _ExtractedFiles;
+        private readonly Dispatcher _Dispatcher;
+        private readonly Window _View;
+        private readonly List<String> _ExtractedFiles;
 
 
 
@@ -224,13 +250,6 @@ namespace AutoUnzip.viewmodel
 
             // Add new theme.
             System.Windows.Application.Current.Resources.MergedDictionaries.Add (dict);
-        }
-
-
-
-        public void OnPropertyChanged (string propertyName)
-        {
-            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
         }
     }
 }
